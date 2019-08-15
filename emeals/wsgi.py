a6faee9ab2c5869cbca6490e37233989abf809fd
Aroldo <aroldo@digital.gob.cl>
2019-08-14 23:28:03 -0400

    Initial definition of Django project, first environment variables and dependency file are included.

---
+++ emeals/wsgi.py
@@ -0,0 +1,9 @@
+"""WSGI config for emeals project."""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emeals.settings')
+
+application = get_wsgi_application()
