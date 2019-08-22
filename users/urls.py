60afc977579d5141667088aea258019c9851623b
Aroldo <aroldo@digital.gob.cl>
2019-08-21 22:18:05 -0400

    Logout view is added

--- users/urls.py
+++ users/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
         view=users_views.login_view,
         name='login'
     ),
+    path(
+        route='logout/',
+        view=users_views.logout_view,
+        name='logout'
+    ),
 ]
--- users/views.py
+++ users/views.py
@@ -1,24 +1,33 @@
 """Users views."""
 
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import (
     authenticate,
-    login
+    login,
+    logout
 )
 from django.shortcuts import render, redirect
 
 
 def login_view(request):
-
+    """Logoin a user."""
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return redirect('/')
+            return redirect('reservations:menu_list')
         else:
             return render(request, 'users/login.html', {
                 'error': 'invalid username and password'
             })
 
     return render(request, 'users/login.html')
+
+
+@login_required
+def logout_view(request):
+    """Logout a user."""
+    logout(request)
+    return redirect('users:logout')
