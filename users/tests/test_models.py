93606b851f2c5e148c1c5d71f61d2a5b038e5b3c
Aroldo <aroldo@digital.gob.cl>
2019-08-26 00:00:27 -0400

    configuration file is added for flake8 use

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -16,10 +16,10 @@ class CreateUserTestCase(TestCase):
 
     def setUp(self):
         self.alan = {
-            'email':'alanbrito@gmail.com',
-            'password':'4D186321C1A7',
-            'first_name':'Alan',
-            'last_name':'Brito'
+            'email': 'alanbrito@gmail.com',
+            'password': '4D186321C1A7',
+            'first_name': 'Alan',
+            'last_name': 'Brito'
         }
         self.alonso = {
             'email': 'alanbrito@gmail.com',
@@ -30,17 +30,14 @@ class CreateUserTestCase(TestCase):
 
     def test_create_user(self):
         """Verify that the user was created correctly."""
-        self.user_alan = User.objects.create(**self.alan)
-        self.assertIsNotNone(self.user_alan.id)
+        user_alan = User.objects.create(**self.alan)
+        self.assertIsNotNone(user_alan.id)
 
     def test_unique_user_creation(self):
         """Proof that you cannot create a user with the same."""
+        User.objects.create(**self.alan)
+
         with self.assertRaises(Exception) as raised:
-            self.user_alan2 = User.objects.create(
-                email='alanbrito@gmail.com',
-                password='61C131A7284D',
-                first_name='Alonso',
-                last_name='Brito'
-            )
+            User.objects.create(**self.alonso)
         # Raise exception duplicate key value violates unique constraint
         self.assertEqual(IntegrityError, type(raised.exception))
